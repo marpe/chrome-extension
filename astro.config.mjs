0a70de8764447d500bec1db0dc3128811b226e15
marpe <marpe@kensei.games>
2024-07-07 17:52:48 +0200

    WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,20 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
+import {crx} from "@crxjs/vite-plugin";
+import manifest from './public/manifest.json' assert {type: 'json'}
 
 // https://astro.build/config
 export default defineConfig({
   build: {
     assets: 'app',
+  },
+  vite: {
+    server: {
+      hmr: {
+        port: 5173,
+      },
+    },
+    plugins: [
+      crx({manifest}),
+    ],
   },
 });
